dac05def2636f952589914590860045a013912f7
Arka Pramanik <arkamtg.pramanik@gmail.com>
2025-09-11 20:12:54 +0530

    Time: 3 ms (7.28%) | Memory: 18.9 MB (93.69%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -4,17 +4,6 @@ class Solution:
         high=len(nums)
         flag=0
         llimit,ulimit=-1,-1
-        while low<high:
-            mid=(low+high)//2
-            if target<nums[mid]:
-                high=mid
-            elif target>nums[mid]:
-                low=mid+1
-            else:
-                flag=1
-                break
-        if flag==0:
-            return [-1,-1]
         low=0
         high=len(nums)
         while low<high:
@@ -26,7 +15,6 @@ class Solution:
             else:
                 llimit=mid
                 high=mid
-        print(mid)
         low=0
         high=len(nums)
         while low<high:
